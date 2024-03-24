923b3769f1e87c7015f39f8e5088e7b98dedfb3f
6lueparr0t <6lueparr0t@gmail.com>
2024-03-25 00:34:55 +0900

    fix : day 2 - 모달 완성도 높임4 minor

--- src/store/modal.tsx
+++ src/store/modal.tsx
@@ -3,12 +3,19 @@ import { Modal } from "@/components/components.d";
 
 type Store = {
   modals: Modal[];
+  clearModals: () => void;
   pushModals: (modal: Modal) => void;
   popModals: () => void;
 };
 
 const modalStore = create<Store>()((set) => ({
   modals: [],
+  clearModals: () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    set((_) => {
+      return { modals: [] };
+    });
+  },
   pushModals: (modal: Modal) => {
     set((state) => {
       state.modals.push(modal);
