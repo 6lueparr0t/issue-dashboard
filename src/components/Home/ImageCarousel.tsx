4fba6ebfb92787b224b4a8bb273a27ee57972684
6lueparr0t <6lueparr0t@gmail.com>
2024-04-02 16:26:45 +0900

    perf : fp 로 변환

--- src/components/Home/ImageCarousel.tsx
+++ src/components/Home/ImageCarousel.tsx
@@ -7,6 +7,8 @@ import {
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 
+import fp from "lodash/fp";
+
 const IMAGES:string[] = [
   'https://picsum.photos/id/83/1280/720.jpg',
   'https://picsum.photos/id/987/1280/720.jpg',
@@ -17,14 +19,11 @@ const IMAGES:string[] = [
 export const ImageCarousel: React.FC = () => {
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState(0);
-  const [, setCount] = useState(IMAGES.length);
 
   useEffect(() => {
     if (!api) return;
 
-    setCount(api.scrollSnapList().length);
     setCurrent(api.selectedScrollSnap() + 1);
-
     api.on("select", () => {
       setCurrent(api.selectedScrollSnap() + 1);
     });
@@ -45,24 +44,24 @@ export const ImageCarousel: React.FC = () => {
       ]}
     >
       <CarouselContent>
-        {IMAGES.map((image, index) => (
-          <CarouselItem key={index}>
+        {fp.map((image) => (
+          <CarouselItem key={image}>
             <div className="border-none shadow-none flex items-center justify-center">
-              <img src={image} alt={`Placeholder Image ${index}`}/>
+              <img src={image} alt={`Placeholder Image ${image}`}/>
             </div>
           </CarouselItem>
-        ))}
+        ), IMAGES)}
       </CarouselContent>
       <div className="flex justify-center mt-4">
-        {IMAGES.map((_, index) => (
+        {fp.map((num) => (
           <span
-            key={index}
+            key={num}
             className={`cursor-pointer inline-block w-3 h-3 rounded-full mx-2 ${
-              index + 1 === current ? "bg-gray-100" : "bg-gray-400"
+              num + 1 === current ? "bg-gray-100" : "bg-gray-400"
             }`}
-            onClick={() => api && api.scrollTo(index)}
+            onClick={() => api && api.scrollTo(num)}
           />
-        ))}
+        ), Array.from({ length: IMAGES.length }, (_, num) => num))}
       </div>
     </Carousel>
   );
--- src/components/_common/Navigation.tsx
+++ src/components/_common/Navigation.tsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 
+import fp from "lodash/fp";
+
 const MENU: { path: string; title: string }[] = [
   {
     path: "/",
@@ -20,15 +22,13 @@ const Navigation: React.FC = () => {
     <header className="w-full mx-auto px-8 py-8 flex justify-between border-solid border-b-2 border-gray-800">
       <nav className="w-screen">
         <ul className="flex gap-4">
-          {MENU.map((item, index) => {
-            return (
-              <li key={index} className="flex-1 text-center">
+          {fp.map((item) => (
+              <li key={item.path} className="flex-1 text-center">
                 <NavLink to={item.path} className={"text-gray-800 hover:text-gray-400"} end>
                   {item.title}
                 </NavLink>
               </li>
-            );
-          })}
+            ), MENU)}
         </ul>
       </nav>
     </header>
